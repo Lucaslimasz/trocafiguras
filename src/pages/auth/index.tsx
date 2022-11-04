e4f26a6881aa9a0731b3afa23f42e4d092e43208
Lucas Lima <id.lucas@outlook.com.br>
2022-11-03 23:02:39 -0300

    creation page auth

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,11 +1,27 @@
 import * as S from './styles';
 
 import MobileIllustration from '../../assets/icons/mobile.svg';
+import LoginIcon from '../../assets/icons/login.svg';
+import Logo from '../../assets/logo-auth.png';
+
+import Input from '../../components/Input';
 
 export default function Auth () {
   return (
     <S.Container>
-      <div></div>
+      <div>
+        <img src={Logo} alt="Logomarca" />
+        <h1>Faça seu login</h1>
+        <form>
+          <Input placeholder="Digite seu login" />
+          <Input placeholder="Digite sua senha" />
+          <button>Entrar</button>
+        </form>
+        <span>
+          <img src={LoginIcon} alt="Entrar" />
+          <strong>Não tenho cadastro</strong>
+        </span>
+      </div>
       <img src={MobileIllustration} alt="" />
     </S.Container>
   );
--- src/pages/auth/styles.ts
+++ src/pages/auth/styles.ts
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  max-width: 70rem;
+  padding: 0 1rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: auto;
+  div {
+    width: 100%;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+    font-size: 2.25rem;
+    color: var(--light);
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    max-width: 22.5rem;
+    width: 100%;
+
+    gap: 0.75rem;
+  }
+`;
